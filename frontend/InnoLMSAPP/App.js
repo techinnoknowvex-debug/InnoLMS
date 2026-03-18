058dde73759e6f856a4c9018c7d5ff42b5a112c2
Narendra <narendrachakka33@gmail.com>
2026-03-18 16:05:57 +0530

    tested the app

--- frontend/InnoLMSAPP/App.js
+++ frontend/InnoLMSAPP/App.js
@@ -1,10 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [data,setData]=useState([]);
+  async function sample(){
+      const res=await fetch("http://192.168.9.121:5000/LMS/course",{
+        method:"GET",
+        headers:{"Content-Type":"application/json"},
+      })
+      const data=await res.json();
+      if(!res.ok){
+        console.log("Error");
+      }else{
+        console.log(data)
+        console.log("Excuted")
+        setData(data.courses);
+      }
+  }
+
   return (
     <View style={styles.container}>
-      <Text></Text>
+      <Text>Hello from innoknowvex</Text>
+      <Pressable onPress={sample}>
+        <Text>Click me</Text>
+        </Pressable>
+        {data.map((course,index)=>(
+            <Text key={index}>{course.title}</Text>
+        ))}
       <StatusBar style="auto" />
     </View>
   );
